b528b616c203a5101dfeeb2473ee25bd0bd54673
Elliot Briant <briantmoliveira@gmail.com>
2018-10-06 22:57:03 -0700

    deleted /receipts route

--- controllers/receipt.js
+++ controllers/receipt.js
@@ -4,6 +4,10 @@ const models = require('../db/models');
 
 module.exports = function(app) {
 
+  app.post('/api/img', (req, res) => {
+
+  });
+
 
 
   //retrieve all items ever scanned
@@ -13,14 +17,9 @@ module.exports = function(app) {
   // }))
 
   //create bulk entry for receipt
-  // app.post('/', (req, res) => {
-  //   // console.log(req.body)
-  //   const receipt = Receipt.bulkCreate(req.body, {returning: true})
   app.post('/', asyncHandler(async (req, res, next) => {
   // console.log(req.body)
   const receipt = await models.Receipt.bulkCreate(req.body, {returning: true})
-    //console.log("********",req.body)
-    // const receipt = await Receipt.findAll()
     if (receipt.length === 0) {
       return res.send('no entries returned').status(500)
     }
@@ -29,19 +28,19 @@ module.exports = function(app) {
 
 
 
-  app.post('/receipts', (req, res) => {
-    var newReceipt = {
-      merchant: req.body.merchant,
-      product: req.body.product,
-      price: req.body.price,
-      date: req.body.date
-    }
-    models.Receipt.create(newReceipt, {w:1}).then((savedReceipt) => {
-      res.status(200).json({message: 'Receipt stored successfully'})
-    }).catch((err) => {
-      res.json(err)
-    })
-  });
+  // app.post('/receipts', (req, res) => {
+  //   var newReceipt = {
+  //     merchant: req.body.merchant,
+  //     product: req.body.product,
+  //     price: req.body.price,
+  //     date: req.body.date
+  //   }
+  //   models.Receipt.create(newReceipt, {w:1}).then((savedReceipt) => {
+  //     res.status(200).json({message: 'Receipt stored successfully'})
+  //   }).catch((err) => {
+  //     res.json(err)
+  //   })
+  // });
 
   app.delete('/:id', asyncHandler(async (req, res, next) => {
     await Receipt.destroy({
