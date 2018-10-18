8417ab53e5c94d6cfde115ec94de0e5f67ba9ff2
Sam Galizia <samgalizia@gmail.com>
2018-10-18 12:49:23 -0700

    Remove git merge lines

--- utils/conversion.js
+++ utils/conversion.js
@@ -3,16 +3,6 @@ const axios = require('axios');
 const uuidv1 = require('uuid/v1');
 const { convertReceiptFromURL } = require('../controllers/taggun');
 
-<<<<<<< HEAD
-
-  /** const receiptURLs = {
-   traderJoe1: 'http://4hatsandfrugal.com/wp-content/uploads/2015/06/64-dollar-grocery-budget-trader-joes1.jpg',
-   kroger1: 'https://thesaraandmalarishow.files.wordpress.com/2009/03/kroger.jpg',
-   foodtown1: 'http://neuseelandbilder.com/en/img/foodtown.gif',
-   fairway1: 'https://www.thebillfold.com/wp-content/uploads/2016/05/1zwdpei1DmTW0V5iyPVOB_A.png',
-   traderJoe2: 'https://birdfriendsnesthomes.files.wordpress.com/2015/02/fullsizerender_12.jpg',
- } */
-=======
 /** const receiptURLs = {
  traderJoe1: 'http://4hatsandfrugal.com/wp-content/uploads/2015/06/64-dollar-grocery-budget-trader-joes1.jpg',
   kroger1: 'https://thesaraandmalarishow.files.wordpress.com/2009/03/kroger.jpg',
@@ -20,7 +10,6 @@ const { convertReceiptFromURL } = require('../controllers/taggun');
   fairway1: 'https://www.thebillfold.com/wp-content/uploads/2016/05/1zwdpei1DmTW0V5iyPVOB_A.png',
   traderJoe2: 'https://birdfriendsnesthomes.files.wordpress.com/2015/02/fullsizerender_12.jpg',
 } */
->>>>>>> Update formatting to adhere to AirBnB style guide
 
 
 /** Processing receipt data and Parsing Merchant Details  */
@@ -64,33 +53,6 @@ const processItemData = (receipt, receiptId) => {
 };
 
 
-<<<<<<< HEAD
-  /** Calling database endpoint to store data */
-  const storeReceiptDataFromURL = async (receiptURL, ocrFunc, processingFunc, user_key) => {
-    const dbReq = axios.create({
-      baseURL: 'http://localhost:8080'
-    })
-    try {
-      const receipt = await ocrFunc(receiptURL)
-      let receiptArr = processingFunc(receipt, user_key, receiptURL)
-      let receipt_id = receiptArr.id
-      let itemsArr = processItemData(receipt, receipt_id)
-      console.log('storing receipt')
-      const storedReceipt = await dbReq.post('/receipt/records', receiptArr)
-      const storedItems = await dbReq.post('/receipt/items', itemsArr)
-      console.log('receipt stored', storedReceipt.data)
-      console.log('items stored', storedItems.data)
-      return storedReceipt.data, storedItems.data
-    } catch (err) {
-      console.log(err.message)
-    }
-  }
-  /** Transport data received */
-  const runapp = async (url, user_ID) => {
-      let user_key = user_ID
-      await storeReceiptDataFromURL(url, convertReceiptFromURL, processReceiptData, user_key)
-    console.log('batch receipt storage complete')
-=======
 /** Calling database endpoint to store data */
 const storeReceiptDataFromURL = async (receiptURL, userKey) => {
   const dbReq = axios.create({
@@ -111,7 +73,6 @@ const storeReceiptDataFromURL = async (receiptURL, userKey) => {
     console.log(err.message);
     return new Error(err.message);
   }
->>>>>>> Update formatting to adhere to AirBnB style guide
 };
 
 /** Transport data received */
