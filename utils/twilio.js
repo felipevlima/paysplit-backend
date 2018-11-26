e663fc9e0c577f3289a172b743fbb8c96721b30b
Briant Oliveira <briantmoliveira@gmail.com>
2018-11-25 16:14:57 -0800

    Fixing twilio deeplink

--- utils/twilio.js
+++ utils/twilio.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const Twilio = require('twilio');
 
 /** Calling Twillio API to text recipients */
-const sendText = async (data, firstName) => {
+const sendText = async (data, userInfo) => {
   const accountSid = process.env.ACCSID;
   const authToken = process.env.TWILIOTOKEN;
   const convertToDecimal = parseFloat(data.amount).toFixed(2);
   const convertMsg = encodeURIComponent(data.msg);
-  const url = `venmo://paycharge?txn=pay&recipients=${data.recipient}&amount=${convertToDecimal}&note=${convertMsg}`;
-  const txtMessage = `Hi, ${firstName} requested ${convertToDecimal} via PaySplit  ${url}`;
+  const url = `venmo://paycharge?txn=pay&recipients=${userInfo.phoneNumber}&amount=${convertToDecimal}&note=${convertMsg}`;
+  const txtMessage = `Hi, ${userInfo.firstName} requested ${convertToDecimal} via PaySplit  ${url}`;
 
   const client = new Twilio(accountSid, authToken);
   const message = await client.messages.create({
