aba37a91e4360bfcf215d5c157a6739463012c26
Briant Oliveira <briantmoliveira@gmail.com>
2018-11-30 08:57:28 -0800

    Changed Heroku Db config

--- db/config/config.js
+++ db/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: process.env.HEROKU_POSTGRESQL_BLACK_URL,
+    use_env_variable: process.env.DATABASE_URL,
     username: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
