ef1814b8910d9b88d83e9ead02860cd372ac3918
Briant Oliveira <briantmoliveira@gmail.com>
2018-12-19 21:28:39 -0800

    Changed index.js

--- db/models/index.js
+++ db/models/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config = require('../config/config.js')[env];
 // const config = require(__dirname + '/../config/config.js')[env];
 
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BLUE_URL, config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
